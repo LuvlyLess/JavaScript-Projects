0a6584514e20519212964747b68c16ea06174746
Leslie <106999007+LuvlyLess@users.noreply.github.com>
2022-07-13 10:51:26 -0700

    Project2_functions AOLCC
    This is my Javascript AOLCC Projects & Submissions for JavaScript

---
+++ Basic-JavaScript-Projects/Project1-expressions-alert/Project2_functions/JS/main.js
@@ -0,0 +1,11 @@
+
+function workffs() {
+    var str = "this text ";
+    str += " is green ";
+    var result = str.fontcolor("green");
+    document.getElementById("green_text").innerHTML = result;
+}
+
+
+
+
